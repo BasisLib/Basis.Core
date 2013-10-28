d79b28d10d5289b4e78c5640772fc681174a0eb3
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-10-29 00:14:57 +0900

    add NameValueBag

---
+++ Basis.Core.Tests/NameValueBagTest.fs
@@ -0,0 +1,319 @@
+﻿namespace Basis.Core.Tests
+
+open NUnit.Framework
+open FsUnit
+
+open Basis.Core.Collections
+
+[<TestFixture>]
+module NameValueBagTest =
+  [<Test>]
+  let empty() =
+    NameValueBag.empty
+    |> NameValueBag.toSeq
+    |> should be Empty
+
+  let testCaseFor_ofSeq (nvs: (string * string list) seq, expected: (string * string list) seq -> (string * string list) seq) = TestCaseData(nvs, expected nvs |> Seq.toList)
+  let src_ofSeq = seq {
+    yield testCaseFor_ofSeq(Seq.empty, id)
+    yield testCaseFor_ofSeq(seq { yield ("key", []) }, fun _ -> Seq.empty)
+    yield testCaseFor_ofSeq(seq { yield ("key", ["value"]) }, id)
+    yield testCaseFor_ofSeq(seq { yield ("key", ["value1"; "value2"]) }, id)
+    yield testCaseFor_ofSeq(seq { yield ("key1", ["value1"]); yield ("key2", ["value2"]) }, id)
+    yield testCaseFor_ofSeq(seq { yield ("key1", []); yield ("key2", ["value2"]); yield ("key1", ["value1"]) }, fun _ -> seq { yield ("key2", ["value2"]); yield ("key1", ["value1"]) })
+  }
+
+  [<TestCaseSource "src_ofSeq">]
+  let ofSeq(nvs: (string * string list) seq, expected: (string * string list) list) =
+    NameValueBag.ofSeq nvs
+    |> NameValueBag.toSeq
+    |> Seq.toList
+    |> should equal expected
+
+  /// NVB []
+  let emptyElemBag = NameValueBag.empty
+  /// NVB [("name", ["value"])]
+  let oneElemBag = NameValueBag.ofSeq [("name", ["value"])]
+  /// NVB [("name1", ["value"]); ("name2", ["value1"; "value2"])]
+  let twoElemBag = NameValueBag.ofSeq [("name1", ["value"]); ("name2", ["value1"; "value2"])]
+
+  let src_addValue = seq {
+    yield TestCaseData(emptyElemBag, "name",  null,     null).Throws(typeof<System.ArgumentNullException>)
+    yield TestCaseData(emptyElemBag, null,    "value",  null).Throws(typeof<System.ArgumentNullException>)
+    yield TestCaseData(emptyElemBag, "name",  "value",  NameValueBag.ofSeq [("name", ["value"])])
+    yield TestCaseData(emptyElemBag, "name",  "other",  NameValueBag.ofSeq [("name", ["other"])])
+    yield TestCaseData(oneElemBag,   "name",  "value",  NameValueBag.ofSeq [("name", ["value"; "value"])])
+    yield TestCaseData(oneElemBag,   "name",  "value2", NameValueBag.ofSeq [("name", ["value"; "value2"])])
+    yield TestCaseData(oneElemBag,   "name2", "value",  NameValueBag.ofSeq [("name", ["value"]); ("name2", ["value"])])
+    yield TestCaseData(twoElemBag,   "name1", "value",  NameValueBag.ofSeq [("name1", ["value"; "value"]); ("name2", ["value1"; "value2"])])
+    yield TestCaseData(twoElemBag,   "name1", "value2", NameValueBag.ofSeq [("name1", ["value"; "value2"]); ("name2", ["value1"; "value2"])])
+  }
+
+  let src_remove = seq {
+    yield TestCaseData(emptyElemBag, "name",  emptyElemBag)
+    yield TestCaseData(oneElemBag,   "name",  emptyElemBag)
+    yield TestCaseData(oneElemBag,   "aaaa",  oneElemBag)
+    yield TestCaseData(twoElemBag,   "name1", NameValueBag.ofSeq [("name2", ["value1"; "value2"])])
+    yield TestCaseData(twoElemBag,   "name2", NameValueBag.ofSeq [("name1", ["value"])])
+    yield TestCaseData(twoElemBag,   "aaaaa", twoElemBag)
+  }
+
+  [<TestCaseSource "src_addValue">]
+  let addValue(initBag: NameValueBag, addingName: string, addingValue: string, expected: NameValueBag) =
+    initBag
+    |> NameValueBag.addValue (addingName, addingValue)
+    |> should equal expected
+
+  [<TestCaseSource "src_remove">]
+  let remove(bag: NameValueBag, name: string, expected: NameValueBag) =
+    bag
+    |> NameValueBag.remove name
+    |> should equal expected
+
+  let testCaseFor_add(initBag, name, values: string list, expected) = TestCaseData(initBag, name, values, expected)
+
+  let src_add = seq {
+    yield testCaseFor_add(emptyElemBag, "name",  [],         emptyElemBag)
+    yield testCaseFor_add(oneElemBag,   "name",  [],         oneElemBag)
+    yield testCaseFor_add(twoElemBag,   "name",  [],         twoElemBag)
+    yield testCaseFor_add(emptyElemBag, "name",  ["value"],  NameValueBag.ofSeq [("name", ["value"])])
+    yield testCaseFor_add(oneElemBag,   "name",  ["value2"], NameValueBag.ofSeq [("name", ["value"; "value2"])])
+    yield testCaseFor_add(twoElemBag,   "name",  ["value"],  NameValueBag.ofSeq [("name1", ["value"]); ("name2", ["value1"; "value2"]); ("name", ["value"])])
+    yield testCaseFor_add(twoElemBag,   "name1", ["value2"], NameValueBag.ofSeq [("name1", ["value"; "value2"]); ("name2", ["value1"; "value2"])])
+    yield testCaseFor_add(emptyElemBag, "other", ["value"],  NameValueBag.ofSeq [("other", ["value"])])
+    yield testCaseFor_add(oneElemBag,   "other", ["value"],  NameValueBag.ofSeq [("name", ["value"]); ("other", ["value"])])
+    yield testCaseFor_add(twoElemBag,   "other", ["value"],  NameValueBag.ofSeq [("name1", ["value"]); ("name2", ["value1"; "value2"]); ("other", ["value"])])
+    yield testCaseFor_add(twoElemBag,   "other", ["value"],  NameValueBag.ofSeq [("name1", ["value"]); ("name2", ["value1"; "value2"]); ("other", ["value"])])
+    yield testCaseFor_add(emptyElemBag, "name",  ["value"; "value2"],  NameValueBag.ofSeq [("name", ["value"; "value2"])])
+    yield testCaseFor_add(oneElemBag,   "name",  ["value2"; "value3"], NameValueBag.ofSeq [("name", ["value"; "value2"; "value3"])])
+    yield testCaseFor_add(twoElemBag,   "name",  ["value"; "value2"],  NameValueBag.ofSeq [("name1", ["value"]); ("name2", ["value1"; "value2"]); ("name", ["value"; "value2"])])
+    yield testCaseFor_add(twoElemBag,   "name1", ["value2"; "value3"], NameValueBag.ofSeq [("name1", ["value"; "value2"; "value3"]); ("name2", ["value1"; "value2"])])
+    yield testCaseFor_add(emptyElemBag, "other", ["value"; "value2"],  NameValueBag.ofSeq [("other", ["value"; "value2"])])
+    yield testCaseFor_add(oneElemBag,   "other", ["value"; "value2"],  NameValueBag.ofSeq [("name", ["value"]); ("other", ["value"; "value2"])])
+    yield testCaseFor_add(twoElemBag,   "other", ["value"; "value2"],  NameValueBag.ofSeq [("name1", ["value"]); ("name2", ["value1"; "value2"]); ("other", ["value"; "value2"])])
+    yield testCaseFor_add(twoElemBag,   "other", ["value"; "value2"],  NameValueBag.ofSeq [("name1", ["value"]); ("name2", ["value1"; "value2"]); ("other", ["value"; "value2"])])
+  }
+
+  [<TestCaseSource "src_add">]
+  let addValues(initBag: NameValueBag, addingName: string, addingValues: string list, expected: NameValueBag) =
+    initBag
+    |> NameValueBag.add (addingName, addingValues)
+    |> should equal expected
+
+  let src_containsName = seq {
+    yield TestCaseData(emptyElemBag, "name", false)
+    yield TestCaseData(oneElemBag, "name", true)
+    yield TestCaseData(oneElemBag, "other", false)
+  }
+
+  [<TestCaseSource "src_containsName">]
+  let containsName(bag: NameValueBag, name: string, expected: bool) =
+    bag
+    |> NameValueBag.containsName name
+    |> should equal expected
+
+  let testCaseFor_exists(bag, pred: (string * string list) -> bool, expected) = TestCaseData(bag, pred, expected)
+  let testCaseFor_forall = testCaseFor_exists
+
+  let src_exists = seq {
+    yield testCaseFor_exists(emptyElemBag, (fun _ -> true),  false)
+    yield testCaseFor_exists(emptyElemBag, (fun _ -> false), false)
+    yield testCaseFor_exists(oneElemBag,   (fun _ -> true),  true)
+    yield testCaseFor_exists(oneElemBag,   (fun _ -> false), false)
+    yield testCaseFor_exists(oneElemBag,   (fun (name, values) -> name.Length + values.Length > 6), false)
+    yield testCaseFor_exists(twoElemBag,   (fun (name, values) -> name.Length + values.Length > 6), true)  // "name2".Length + ["value1"; "value2"].Length = 7
+    yield testCaseFor_exists(twoElemBag,   (fun (name, values) -> name.Length + values.Length > 7), false)
+  }
+
+  let src_forall = seq {
+    yield testCaseFor_forall(emptyElemBag, (fun _ -> true),  true)
+    yield testCaseFor_forall(emptyElemBag, (fun _ -> false), true)
+    yield testCaseFor_forall(oneElemBag,   (fun _ -> true),  true)
+    yield testCaseFor_forall(oneElemBag,   (fun _ -> false), false)
+    yield testCaseFor_forall(oneElemBag,   (fun (name, values) -> name.Length + values.Length < 8), true)
+    yield testCaseFor_forall(twoElemBag,   (fun (name, values) -> name.Length + values.Length < 8), true)  // "name2".Length + ["value1"; "value2"].Length = 7
+    yield testCaseFor_forall(twoElemBag,   (fun (name, values) -> name.Length + values.Length < 7), false)
+  }
+
+  [<TestCaseSource "src_exists">]
+  let exists(bag: NameValueBag, pred: (string * string list) -> bool, expected: bool) =
+    bag
+    |> NameValueBag.exists pred
+    |> should equal expected
+
+  [<TestCaseSource "src_forall">]
+  let forall(bag: NameValueBag, pred: (string * string list) -> bool, expected: bool) =
+    bag
+    |> NameValueBag.forall pred
+    |> should equal expected
+
+  let testCaseFor_filter(bag, pred: (string * string list) -> bool, expected) = TestCaseData(bag, pred, expected)
+  let testCaseFor_partition = testCaseFor_filter
+
+  let src_filter = seq {
+    yield testCaseFor_filter(emptyElemBag, (fun _ -> true),  emptyElemBag)
+    yield testCaseFor_filter(emptyElemBag, (fun _ -> false), emptyElemBag)
+    yield testCaseFor_filter(oneElemBag,   (fun _ -> true),  oneElemBag)
+    yield testCaseFor_filter(oneElemBag,   (fun _ -> false), emptyElemBag)
+    yield testCaseFor_filter(oneElemBag,   (fun (name, values) -> name.Length + values.Length > 6), emptyElemBag)
+    yield testCaseFor_filter(twoElemBag,   (fun (name, values) -> name.Length + values.Length > 6), NameValueBag.ofSeq [("name2", ["value1"; "value2"])]) // "name2".Length + ["value1"; "value2"].Length = 7
+    yield testCaseFor_filter(twoElemBag,   (fun (name, values) -> name.Length + values.Length > 7), emptyElemBag)
+  }
+
+  let src_partition = seq {
+    yield testCaseFor_partition(emptyElemBag, (fun _ -> true),  (emptyElemBag, emptyElemBag))
+    yield testCaseFor_partition(emptyElemBag, (fun _ -> false), (emptyElemBag, emptyElemBag))
+    yield testCaseFor_partition(oneElemBag,   (fun _ -> true),  (oneElemBag, emptyElemBag))
+    yield testCaseFor_partition(oneElemBag,   (fun _ -> false), (emptyElemBag, oneElemBag))
+    yield testCaseFor_partition(oneElemBag,   (fun (name, values) -> name.Length + values.Length > 6),
+                                                                (emptyElemBag, oneElemBag))
+    yield testCaseFor_partition(twoElemBag,   (fun (name, values) -> name.Length + values.Length > 6),
+                                                                (NameValueBag.ofSeq [("name2", ["value1"; "value2"])], NameValueBag.ofSeq [("name1", ["value"])])) // "name2".Length + ["value1"; "value2"].Length = 7
+    yield testCaseFor_partition(twoElemBag,   (fun (name, values) -> name.Length + values.Length > 7),
+                                                                (emptyElemBag, twoElemBag))
+  }
+
+  [<TestCaseSource "src_filter">]
+  let filter(bag: NameValueBag, pred: (string * string list) -> bool, expected: NameValueBag) =
+    bag
+    |> NameValueBag.filter pred
+    |> should equal expected
+
+  [<TestCaseSource "src_partition">]
+  let partition(bag: NameValueBag, pred: (string * string list) -> bool, expected: NameValueBag * NameValueBag) =
+    bag
+    |> NameValueBag.partition pred
+    |> should equal expected
+
+  let src_get = seq {
+    yield TestCaseData(emptyElemBag, "name",  null).Throws(typeof<KeyNotFoundException>)
+    yield TestCaseData(oneElemBag,   "name",  ["value"])
+    yield TestCaseData(oneElemBag,   "other", null).Throws(typeof<KeyNotFoundException>)
+    yield TestCaseData(twoElemBag,   "name1", ["value"])
+    yield TestCaseData(twoElemBag,   "name2", ["value1"; "value2"])
+    yield TestCaseData(twoElemBag,   "other", null).Throws(typeof<KeyNotFoundException>)
+  }
+
+  let src_getAsSingleString= seq {
+    yield TestCaseData(emptyElemBag, "name",  null).Throws(typeof<KeyNotFoundException>)
+    yield TestCaseData(oneElemBag,   "name",  "value")
+    yield TestCaseData(oneElemBag,   "other", null).Throws(typeof<KeyNotFoundException>)
+    yield TestCaseData(twoElemBag,   "name1", "value")
+    yield TestCaseData(twoElemBag,   "name2", "value1,value2")
+    yield TestCaseData(twoElemBag,   "other", null).Throws(typeof<KeyNotFoundException>)
+  }
+
+  [<TestCaseSource "src_get">]
+  let get(bag: NameValueBag, name: string, expected: string list) =
+    bag
+    |> NameValueBag.get name
+    |> should equal expected
+
+  [<TestCaseSource "src_getAsSingleString">]
+  let getAsSingleString(bag: NameValueBag, name: string, expected: string) =
+    bag
+    |> NameValueBag.getAsSingleString name
+    |> should equal expected
+
+  let testCaseFor_find(bag, pred: (string * string list) -> bool, expected: string list) =
+    let data = TestCaseData(bag, pred, expected)
+    if (box expected) = null then data.Throws(typeof<KeyNotFoundException>) else data
+  let testCaseFor_findName(bag, pred: (string * string list) -> bool, expected: string) =
+    let data = TestCaseData(bag, pred, expected)
+    if expected = null then data.Throws(typeof<KeyNotFoundException>) else  data
+  let testCaseFor_pick(bag, chooser: (string * string list) -> string option, expected: string) =
+    let data = TestCaseData(bag, chooser, expected)
+    if expected = null then data.Throws(typeof<KeyNotFoundException>) else data
+
+  let src_find = seq {
+    yield testCaseFor_find(emptyElemBag, (fun _ -> true),  Unchecked.defaultof<string list>)
+    yield testCaseFor_find(emptyElemBag, (fun _ -> false), Unchecked.defaultof<string list>)
+    yield testCaseFor_find(oneElemBag,   (fun _ -> true),  ["value"])
+    yield testCaseFor_find(oneElemBag,   (fun _ -> false), Unchecked.defaultof<string list>)
+    yield testCaseFor_find(oneElemBag,   (fun (name, values) -> name.Length + values.Length > 6), Unchecked.defaultof<string list>)
+    yield testCaseFor_find(twoElemBag,   (fun (name, values) -> name.Length + values.Length > 6), ["value1"; "value2"]) // "name2".Length + ["value1"; "value2"].Length = 7
+    yield testCaseFor_find(twoElemBag,   (fun (name, values) -> name.Length + values.Length > 7), Unchecked.defaultof<string list>)
+  }
+
+  let src_findName = seq {
+    yield testCaseFor_findName(emptyElemBag, (fun _ -> true),  null)
+    yield testCaseFor_findName(emptyElemBag, (fun _ -> false), null)
+    yield testCaseFor_findName(oneElemBag,   (fun _ -> true),  "name")
+    yield testCaseFor_findName(oneElemBag,   (fun _ -> false), null)
+    yield testCaseFor_findName(oneElemBag,   (fun (name, values) -> name.Length + values.Length > 6), null)
+    yield testCaseFor_findName(twoElemBag,   (fun (name, values) -> name.Length + values.Length > 6), "name2") // "name2".Length + ["value1"; "value2"].Length = 7
+    yield testCaseFor_findName(twoElemBag,   (fun (name, values) -> name.Length + values.Length > 7), null)
+  }
+
+  let src_pick = seq {
+    yield testCaseFor_pick(emptyElemBag, (fun _ -> Some "a"), null)
+    yield testCaseFor_pick(emptyElemBag, (fun _ -> None),     null)
+    yield testCaseFor_pick(oneElemBag,   (fun _ -> Some "a"), "a")
+    yield testCaseFor_pick(oneElemBag,   (fun _ -> None),     null)
+    yield testCaseFor_pick(oneElemBag,   (fun (name, values) -> if name.Length + values.Length > 6 then Some "a" else None),
+                                                              null)
+    yield testCaseFor_pick(twoElemBag,   (fun (name, values) -> if name.Length + values.Length > 6 then Some "a" else None),
+                                                              "a") // "name2".Length + ["value1"; "value2"].Length = 7
+    yield testCaseFor_pick(twoElemBag,   (fun (name, values) -> if name.Length + values.Length > 7 then Some "a" else None),
+                                                              null)
+  }
+
+  [<TestCaseSource "src_find">]
+  let find(bag: NameValueBag, pred: (string * string list) -> bool, expected: string list) =
+    bag
+    |> NameValueBag.find pred
+    |> should equal expected
+
+  [<TestCaseSource "src_findName">]
+  let findName(bag: NameValueBag, pred: (string * string list) -> bool, expected: string) =
+    bag
+    |> NameValueBag.findName pred
+    |> should equal expected
+
+  [<TestCaseSource "src_pick">]
+  let pick(bag: NameValueBag, chooser: (string * string list) -> string option, expected: string) =
+    bag
+    |> NameValueBag.pick chooser
+    |> should equal expected
+
+  let testCaseFor_fold(bag, f: 'a -> (string * string list) -> 'a, init: 'a, expected: 'a) =
+    TestCaseData(bag, f, init, expected)
+
+  let src_fold = seq {
+    yield testCaseFor_fold(emptyElemBag, (fun _ -> failwith "oops!"), 10, 10)
+    yield testCaseFor_fold(oneElemBag,   (fun acc (name, values) -> name.Length + values.Length + acc), 0, "name".Length + 1) // name: [value]
+    yield testCaseFor_fold(twoElemBag,   (fun acc (name, values) -> name.Length + values.Length + acc), 0, "name1name2".Length + 3) // name1: [value]; name2: [value1; value2]
+  }
+
+  [<TestCaseSource "src_fold">]
+  let fold(bag: NameValueBag, f: 'a -> (string * string list) -> 'a, init: 'a, expected: 'a) =
+    bag
+    |> NameValueBag.fold f init
+    |> should equal expected
+
+  let testCaseFor_foldBack(bag, f: (string * string list) -> 'a -> 'a, init: 'a, expected: 'a) =
+    TestCaseData(bag, f, init, expected)
+
+  let src_foldBack = seq {
+    yield testCaseFor_foldBack(emptyElemBag, (fun _ -> failwith "oops!"), 10, 10)
+    yield testCaseFor_foldBack(oneElemBag,   (fun (name, values) acc -> name.Length + values.Length + acc), 0, "name".Length + 1) // name: [value]
+    yield testCaseFor_foldBack(twoElemBag,   (fun (name, values) acc -> name.Length + values.Length + acc), 0, "name1name2".Length + 3) // name1: [value]; name2: [value1; value2]
+  }
+    
+  [<TestCaseSource "src_foldBack">]
+  let foldBack(bag: NameValueBag, f: (string * string list) -> 'a -> 'a, init: 'a, expected: 'a) =
+    NameValueBag.foldBack f bag init |> should equal expected
+
+  let testCaseFor_map(bag, f: (string * string list) -> string list, expected) = TestCaseData(bag, f, expected)
+
+  let src_map = seq {
+    yield testCaseFor_map(emptyElemBag, (fun _ -> failwith "oops!"), emptyElemBag)
+    yield testCaseFor_map(oneElemBag,   (fun (n, vs) -> vs |> List.rev), NameValueBag.ofSeq [("name", ["value"])])
+    yield testCaseFor_map(twoElemBag,   (fun (n, vs) -> vs |> List.rev), NameValueBag.ofSeq [("name1", ["value"]); ("name2", ["value2"; "value1"])])
+  }
+
+  [<TestCaseSource "src_map">]
+  let map(bag: NameValueBag, f: (string * string list) -> string list, expected: NameValueBag) =
+    bag
+    |> NameValueBag.map f
+    |> should equal expected
---
+++ Basis.Core/NameValueBag.fs
@@ -0,0 +1,195 @@
+﻿namespace Basis.Core.Collections
+
+open System
+open System.Collections.Specialized
+
+type KeyNotFoundException = System.Collections.Generic.KeyNotFoundException
+
+[<Sealed>]
+type NameValueBag (nvs: NameValueCollection) =
+  let nameWithValue (name: string) = (name, nvs.GetValues(name) |> Array.toList)
+  static member private SeqToNameValueCollection(xs) =
+    let nvs = NameValueCollection()
+    for name, values in xs do
+      for value in values do
+        if value <> null then
+          nvs.Add(name, value)
+    nvs
+  new(xs: (string * string list) seq) = NameValueBag(NameValueBag.SeqToNameValueCollection(xs))
+  new(other: NameValueBag) = NameValueBag(other.CloneRawData(): NameValueCollection)
+  member private this.Nvs = nvs
+  member internal this.CloneRawData() = NameValueCollection(nvs)
+  member internal this.Clone(act) =
+    let newRawData = NameValueCollection(nvs)
+    act newRawData
+    NameValueBag(newRawData)
+  member this.ToEnumerable() = nvs.AllKeys |> Array.map nameWithValue :> seq<_>
+  member this.ToArray() = nvs.AllKeys |> Array.map nameWithValue
+  member this.ToFSharpList() = nvs.AllKeys |> Array.map nameWithValue |> Array.toList
+  member this.IsEmpty = nvs.HasKeys
+  member this.AllNames = nvs.AllKeys |> Array.toList
+  member internal this.GetValues(name: string) = nvs.GetValues(name) |> Array.toList
+  member this.AddValue(name, value) =
+    if name = null then nullArg "name"
+    if value = null then nullArg "value"
+    this.Clone(fun nvs -> nvs.Add(name, value))
+  member this.Add(name, values) =
+    this.Clone(fun nvs -> values |> List.iter (fun v -> if v <> null then nvs.Add(name, v)))
+  member this.ContainsName(name: string) = nvs.AllKeys |> Array.exists ((=)name)
+  member this.Exists(pred: (string * string list) -> bool) =
+    nvs.AllKeys
+    |> Seq.map nameWithValue
+    |> Seq.exists pred
+  member this.Forall(pred: (string * string list) -> bool) =
+    nvs.AllKeys
+    |> Seq.map nameWithValue
+    |> Seq.forall pred
+  member this.Filter(pred: (string * string list) -> bool) =
+    let newRawData = this.CloneRawData()
+    let removeKeys = newRawData.AllKeys |> Seq.filter (nameWithValue >> pred >> not)
+    removeKeys |> Seq.iter (newRawData.Remove)
+    NameValueBag(newRawData)
+  member this.Partition(pred: (string * string list) -> bool) =
+    let trues = this.CloneRawData()
+    let falses = this.CloneRawData()
+    for name in nvs.AllKeys do
+      if pred (nameWithValue name)
+        then falses.Remove(name)
+        else trues.Remove(name)
+    (NameValueBag(trues), NameValueBag(falses))
+  member this.TryGetAsSingleString(name: string) =
+    match this.Nvs.Get(name) with null -> None | other -> Some other
+  member this.TryGet(name: string) =
+    nvs.AllKeys |> Array.tryFind ((=)name)
+    |> Option.map (fun searched -> this.GetValues(searched))
+  member this.TryFind(pred: (string * string list) -> bool) =
+    nvs.AllKeys
+    |> Seq.map nameWithValue
+    |> Seq.tryFind pred
+    |> Option.map snd
+  member this.TryFindName(pred: (string * string list) -> bool) =
+    nvs.AllKeys
+    |> Seq.map nameWithValue
+    |> Seq.tryPick (fun ((name, _) as kv) -> if pred kv then Some name else None)
+  member this.TryPick(chooser: (string * string list) -> 'U option) =
+    nvs.AllKeys
+    |> Array.tryPick (nameWithValue >> chooser)
+  member this.Fold(f: 'TState -> string * string list -> 'TState, init: 'TState) =
+    nvs.AllKeys
+    |> Array.fold (fun acc name -> f acc (nameWithValue name)) init
+  member this.FoldBack(f: string * string list -> 'TState -> 'TState, init: 'TState) =
+    Array.foldBack (fun name rest -> f (nameWithValue name) rest) nvs.AllKeys init
+  member this.Map(f: string * string list -> string list) =
+    nvs.AllKeys
+    |> Seq.map (nameWithValue >> (fun (n, vs) -> (n, f (n, vs))))
+    |> (fun xs -> NameValueBag(xs))
+  member this.Iter(act: string * string list -> unit) =
+    nvs.AllKeys
+    |> Array.iter (nameWithValue >> act)
+
+  override this.Equals(obj) =
+    match obj with
+    | :? NameValueBag as other ->
+        let nvs1, nvs2 = this.Nvs, other.Nvs
+        if nvs1.AllKeys = nvs2.AllKeys
+          then (nvs1.AllKeys, nvs2.AllKeys) ||> Array.forall2 (fun n1 n2 -> nvs1.GetValues(n1) = nvs2.GetValues(n2))
+          else false
+    | _ -> false
+  override this.GetHashCode() =
+    nvs.GetHashCode()
+  override this.ToString() =
+    let buf = Text.StringBuilder()
+    buf.Append("NameValueBag {") |> ignore
+    buf.Append(
+      String.Join(
+        "; ",
+        nvs.AllKeys |> Seq.map (fun name -> name + ": [" + String.Join("; ", match nvs.GetValues(name) with null -> [||] | other -> other) + "]"))
+    ) |> ignore
+    buf.Append("}") |> ignore
+    buf.ToString()
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module NameValueBag =
+  [<CompiledName "Empty">]
+  let empty = NameValueBag(NameValueCollection())
+
+  [<CompiledName "IsEmpty">]
+  let isEmpty (bag: NameValueBag) = bag.IsEmpty
+
+  [<CompiledName "ToEnumerable">]
+  let toSeq (bag: NameValueBag) = bag.ToEnumerable()
+
+  [<CompiledName "ToArray">]
+  let toArray (bag: NameValueBag) = bag.ToArray()
+
+  [<CompiledName "ToFSharpList">]
+  let toList (bag: NameValueBag) = bag.ToFSharpList()
+
+  [<CompiledName "OfEnumerable">]
+  let ofSeq (xs: (string * string list) seq) = NameValueBag(xs)
+
+  [<CompiledName "AddValue">]
+  let addValue (name, value) (bag: NameValueBag) = bag.AddValue(name, value)
+
+  [<CompiledName "Add">]
+  let add (name, values) (bag: NameValueBag) = bag.Add(name, values)
+
+  [<CompiledName "Remove">]
+  let remove name (bag: NameValueBag) = bag.Clone(fun nvs -> nvs.Remove(name))
+
+  [<CompiledName "ContainsName">]
+  let containsName name (bag: NameValueBag) = bag.ContainsName(name)
+
+  [<CompiledName "Exists">]
+  let exists pred (bag: NameValueBag) = bag.Exists(pred)
+
+  [<CompiledName "Forall">]
+  let forall pred (bag: NameValueBag) = bag.Forall(pred)
+
+  [<CompiledName "Filter">]
+  let filter pred (bag: NameValueBag) = bag.Filter(pred)
+
+  [<CompiledName "Partition">]
+  let partition pred (bag: NameValueBag) = bag.Partition(pred)
+
+  [<CompiledName "TryGet">]
+  let tryGet name (bag: NameValueBag) = bag.TryGet(name)
+
+  [<CompiledName "Get">]
+  let get name (bag: NameValueBag) = match bag.TryGet(name) with Some values -> values | None -> raise (KeyNotFoundException())
+
+  [<CompiledName "TryGetAsSingleString">]
+  let tryGetAsSingleString name (bag: NameValueBag) = bag.TryGetAsSingleString(name)
+
+  [<CompiledName "GetAsSingleString">]
+  let getAsSingleString name (bag: NameValueBag) = match bag.TryGetAsSingleString(name) with Some str -> str | None -> raise (KeyNotFoundException())
+
+  [<CompiledName "TryFind">]
+  let tryFind pred (bag: NameValueBag) = bag.TryFind(pred)
+
+  [<CompiledName "Find">]
+  let find pred (bag: NameValueBag) = match bag.TryFind(pred) with Some values -> values | None -> raise (KeyNotFoundException())
+
+  [<CompiledName "TryFindName">]
+  let tryFindName pred (bag: NameValueBag) = bag.TryFindName(pred)
+
+  [<CompiledName "FindName">]
+  let findName pred (bag: NameValueBag) = match bag.TryFindName(pred) with Some name -> name | None -> raise (KeyNotFoundException())
+
+  [<CompiledName "TryPick">]
+  let tryPick chooser (bag: NameValueBag) = bag.TryPick(chooser)
+
+  [<CompiledName "Pick">]
+  let pick chooser (bag: NameValueBag) = match bag.TryPick(chooser) with Some res -> res | None -> raise (KeyNotFoundException())
+
+  [<CompiledName "Fold">]
+  let fold f init (bag: NameValueBag) = bag.Fold(f, init)
+
+  [<CompiledName "FoldBack">]
+  let foldBack f (bag: NameValueBag) init = bag.FoldBack(f, init)
+
+  [<CompiledName "Map">]
+  let map f (bag: NameValueBag) = bag.Map(f)
+
+  [<CompiledName "Iter">]
+  let iter act (bag: NameValueBag) = bag.Iter(act)
