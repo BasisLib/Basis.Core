847b439ceb5aba2b5276c185d617e33f302d6f75
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-10-29 16:59:05 +0900

    add 'return!' support (#1)

--- Basis.Core/Option.fs
+++ Basis.Core/Option.fs
@@ -5,6 +5,7 @@ open System
 module Option =
   type OptionBuilder internal () =
     member this.Return(x) = Some x
+    member this.ReturnFrom(x: _ option) = x
     member this.Bind(x, f) = Option.bind f x
 
 [<AutoOpen>]
