58e230abf854fbb03f730477bcd36f8eecbab331
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-10-29 17:30:40 +0900

    add zero support

--- Basis.Core/Option.fs
+++ Basis.Core/Option.fs
@@ -4,6 +4,7 @@ open System
 
 module Option =
   type OptionBuilder internal () =
+    member this.Zero() = None
     member this.Return(x) = Some x
     member this.ReturnFrom(x: _ option) = x
     member this.Bind(x, f) = Option.bind f x
