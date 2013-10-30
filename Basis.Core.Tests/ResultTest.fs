819f649fe3cbdb002cdfb39bf347537e8913ef6a
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-10-30 11:56:53 +0900

    add Result type. (#2)

---
+++ Basis.Core.Tests/ResultTest.fs
@@ -0,0 +1,37 @@
+﻿namespace Basis.Core.Tests
+
+open NUnit.Framework
+open FsUnit
+
+open Basis.Core
+
+[<TestFixture>]
+module ResultTest =
+  let success x : Result<int, string> = Success x
+  let failure x : Result<int, string> = Failure x
+
+  let src_toOption = [
+    TestCaseData(success 10,     Some 10)
+    TestCaseData(success 20,     Some 20)
+    TestCaseData(failure "hoge", None)
+    TestCaseData(failure "piyo", None)
+  ]
+
+  let src_toOptionFailure = [
+    TestCaseData(success 10,     None)
+    TestCaseData(success 20,     None)
+    TestCaseData(failure "hoge", Some "hoge")
+    TestCaseData(failure "piyo", Some "piyo")
+  ]
+
+  [<TestCaseSource "src_toOption">]
+  let toOption (x: Result<int, string>, expected: int option) =
+    x
+    |> Result.toOption
+    |> should equal expected
+
+  [<TestCaseSource "src_toOptionFailure">]
+  let toOptionFailure (x: Result<int, string>, expected: string option) =
+    x
+    |> Result.toOptionFailure
+    |> should equal expected
---
+++ Basis.Core/Result.fs
@@ -0,0 +1,16 @@
+﻿namespace Basis.Core
+
+type Result<'TSuccess, 'TFailure> =
+  | Success of 'TSuccess
+  | Failure of 'TFailure
+with
+  member this.ToOption() = match this with Success s -> Some s | _ -> None
+  member this.ToOptionFailure() = match this with Failure f -> Some f | _ -> None
+  override this.ToString() = sprintf "%A" this
+
+module Result =
+  [<CompiledName "ToOption">]
+  let toOption (result: Result<_, _>) = result.ToOption()
+
+  [<CompiledName "ToOptionFailure">]
+  let toOptionFailure (result: Result<_, _>) = result.ToOptionFailure()
