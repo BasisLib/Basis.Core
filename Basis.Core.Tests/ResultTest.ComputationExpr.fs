b9daf76668519e4fd469ab10ad46ebce25465166
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-10-31 21:28:16 +0900

    add 'return!' support (#3)

--- Basis.Core.Tests/ResultTest.ComputationExpr.fs
+++ Basis.Core.Tests/ResultTest.ComputationExpr.fs
@@ -18,3 +18,13 @@ module ResultComputationExprTest =
   let retret () =
     let res = result { return 1; return 2 }
     res |> should equal (Success 1)
+
+  let src_retFrom = [
+    TestCaseData(Failure "hoge" : Result<int, string>)
+    TestCaseData(Success 10 : Result<int, string>)
+  ]
+
+  [<TestCaseSource "src_retFrom">]
+  let retFrom(x: Result<int, string>) =
+    let res = result { return! x }
+    res |> should equal x
