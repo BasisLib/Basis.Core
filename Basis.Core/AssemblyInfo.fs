f1bee3dddb27146df9d53ad64deff27b873deaed
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-11-01 10:46:55 +0900

    1.0-beta1

--- Basis.Core/AssemblyInfo.fs
+++ Basis.Core/AssemblyInfo.fs
@@ -18,6 +18,6 @@ open System.Runtime.InteropServices
 [<assembly: Guid("EC49895C-DFEF-4973-BCC8-8FAEE657E181")>]
 
 [<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyInformationalVersion("1.0.0-alpha1")>]
+[<assembly: AssemblyInformationalVersion("1.0.0-beta1")>]
 
 ()
