7489cd2b396304b737c756fc49cc2f33f925e207
s.miyazaki <s.miyazaki@otr-jp.com>
2013-12-12 17:25:09 +0900

    fix XPath.evaluate (#32)

---
+++ Basis.Core.Tests/XPathTest.fs
@@ -0,0 +1,74 @@
+﻿module XPathTest
+
+open NUnit.Framework
+open FsUnit
+
+open System.Xml.Linq
+open Basis.Core.Xml.NamespaceLess
+
+[<TestFixture>]
+module XPathEvaluateTest =
+  [<Test>]
+  let ``evaluate string``() =
+    let xpath = """concat("a", "b")"""
+    let xml = XDocument.parse """<xml />"""
+    let actual = XPath.evaluate xpath xml
+    actual |> should equal (XPath.Str "ab")
+
+  [<Test>]
+  let ``evaluate bool``() =
+    let xpath = """starts-with("aaa", "b")"""
+    let xml = XDocument.parse """<xml />"""
+    let actual = XPath.evaluate xpath xml
+    actual |> should equal (XPath.Bool false)
+
+  [<Test>]
+  let ``evaluate number``() =
+    let xpath = """string-length("aaa")"""
+    let xml = XDocument.parse """<xml />"""
+    let actual = XPath.evaluate xpath xml
+    actual |> should (equalWithin 0.1) (XPath.Num 3.0)
+
+  [<Test>]
+  let ``evaluate element seq``() =
+    let xpath = """/xml/a"""
+    let xml = XDocument.parse """<xml><a /><a /><a /></xml>"""
+    let actual = XPath.evaluate xpath xml
+    actual |> should equal (XPath.ElemSeq [ Xml.elem "a" []; Xml.elem "a" []; Xml.elem "a" [] ])
+
+  [<Test>]
+  let ``evaluate empty element seq``() =
+    let xpath = """/xml/b"""
+    let xml = XDocument.parse """<xml><a /><a /><a /></xml>"""
+    let actual = XPath.evaluate xpath xml
+    actual |> should equal XPath.EmptySeq
+
+  [<Test>]
+  let ``evaluate attribute seq``() =
+    let xpath = """/xml/a/@attr"""
+    let xml = XDocument.parse """<xml><a attr="a"/><a attr="b"/><a attr="c"/></xml>"""
+    let actual = XPath.evaluate xpath xml
+    actual |> should equal (XPath.AttrSeq [ Xml.attr "attr" "a"; Xml.attr "attr" "b"; Xml.attr "attr" "c" ])
+
+  [<Test>]
+  let ``evaluate empty attribute seq``() =
+    let xpath = """/xml/b/@attr"""
+    let xml = XDocument.parse """<xml><a attr="a"/><a attr="b"/><a attr="c"/></xml>"""
+    let actual = XPath.evaluate xpath xml
+    actual |> should equal XPath.EmptySeq
+
+  let text (t: string) = XText(t)
+
+  [<Test>]
+  let ``evaluate text seq``() =
+    let xpath = """/xml/a/text()"""
+    let xml = XDocument.parse """<xml><a>a</a><a>b</a><a>c</a></xml>"""
+    let actual = XPath.evaluate xpath xml
+    actual |> should equal (XPath.TextSeq [ text "a"; text "b"; text "c" ])
+
+  [<Test>]
+  let ``evaluate empty text seq``() =
+    let xpath = """/xml/b/text()"""
+    let xml = XDocument.parse """<xml><a /><a /><a /></xml>"""
+    let actual = XPath.evaluate xpath xml
+    actual |> should equal XPath.EmptySeq
--- Basis.Core/XPath.fs
+++ Basis.Core/XPath.fs
@@ -14,6 +14,7 @@ module XPath =
     | notNull -> notNull
   let selectElements xpath (node: #XNode) = node.XPathSelectElements(xpath)
 
+  [<CustomEquality; NoComparison>]
   type EvaluateResult =
     | Bool of bool
     | Num of float
@@ -21,6 +22,39 @@ module XPath =
     | EmptySeq
     | AttrSeq of XAttribute seq
     | ElemSeq of XElement seq
+    | TextSeq of XText seq
+  with
+    override this.Equals(obj) =
+      match obj with
+      | :? EvaluateResult as other ->
+        match this, other with
+        | ElemSeq left, ElemSeq right ->
+          if Seq.length left <> Seq.length right then
+            false
+          else
+            Seq.zip left right |> Seq.forall (fun (l, r) -> XElement.DeepEquals(l, r))
+        | AttrSeq left, AttrSeq right ->
+          if Seq.length left <> Seq.length right then
+            false
+          else
+            Seq.zip left right |> Seq.forall (fun (l, r) -> l.Name = r.Name && l.Value = r.Value)
+        | TextSeq left, TextSeq right ->
+          if Seq.length left <> Seq.length right then
+            false
+          else
+            Seq.zip left right |> Seq.forall (fun (l, r) -> XElement.DeepEquals(l, r))
+        | Bool left, Bool right -> left = right
+        | Num left, Num right -> left = right
+        | Str left, Str right -> left = right
+        | EmptySeq, EmptySeq -> true
+        | _ -> false
+      | _ -> false
+    override this.GetHashCode() =
+      match this with
+      | AttrSeq _ -> 1
+      | ElemSeq _ -> 2
+      | TextSeq _ -> 3
+      | _ -> hash this
 
   let evaluate xpath (node: #XNode) =
     match node.XPathEvaluate(xpath) with
@@ -28,7 +62,11 @@ module XPath =
     | :? bool as b -> Bool b
     | :? float as f -> Num f
     | :? string as s -> Str s
-    | :? System.Collections.IEnumerable as xs when xs |> Seq.cast<obj> |> Seq.isEmpty -> EmptySeq
-    | :? seq<XAttribute> as attrs -> AttrSeq attrs
-    | :? seq<XElement> as elems -> ElemSeq elems
+    | :? seq<obj> as xs when xs |> Seq.isEmpty -> EmptySeq
+    | :? seq<obj> as xs ->
+      match Seq.head xs with
+      | :? XElement -> ElemSeq (Seq.cast<XElement> xs)
+      | :? XAttribute -> AttrSeq (Seq.cast<XAttribute> xs)
+      | :? XText -> TextSeq (Seq.cast<XText> xs)
+      | _ -> failwithf "oops! XPathEvaluate returns %s!" (xs.GetType().FullName)
     | other -> failwithf "oops! XPathEvaluate returns %s!" (other.GetType().FullName)
