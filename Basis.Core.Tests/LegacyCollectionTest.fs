8cf1dd2decd3a16ef1d82e4f0b72ab05acdafc72
hafuu <hafuu46@gmail.com>
2014-08-06 18:51:58 +0900

    add LegacyCollection module refs #15
    legacy collections to seq, list, and array
    NameValueCollection to map<string, string list>

---
+++ Basis.Core.Tests/LegacyCollectionTest.fs
@@ -0,0 +1,29 @@
+﻿namespace Basis.Core.Tests
+
+open NUnit.Framework
+open FsUnit
+
+open Basis.Core
+
+open System.Collections.Specialized
+
+[<TestFixture>]
+module LegacyCollectionTest =
+  [<Test>]
+  let ``toList test``() =
+    let xs = NameValueCollection()
+    xs.Add("x", "1")
+    xs.Add("x", "2")
+    xs.Add("y", "3")
+
+    LegacyCollection.toList xs |> should equal [ "x"; "y" ]
+
+  [<Test>]
+  let ``nameValueCollectionToMap test``() =
+    let xs = NameValueCollection()
+    xs.Add("x", "1")
+    xs.Add("x", "2")
+    xs.Add("y", "3")
+
+    let actual = LegacyCollection.nameValueCollectionToMap xs
+    Map.toList actual |> should equal [ ("x", [ "1"; "2" ]); ("y", [ "3" ]) ]
---
+++ Basis.Core/LegacyCollection.fs
@@ -0,0 +1,30 @@
+﻿module Basis.Core.LegacyCollection
+
+open System.Collections
+open System.Collections.Specialized
+
+let inline toSeq (xs: ^TCollection) : 'T seq when ^TCollection : (member get_Item : int -> 'T) and ^TCollection :> ICollection =
+  seq {
+    let e = xs.GetEnumerator()
+    while e.MoveNext() do
+      yield e.Current :?> 'T
+  }
+
+let inline toList (xs: ^TCollection) : 'T list when ^TCollection : (member get_Item : int -> 'T) and ^TCollection :> ICollection =
+  [
+    let e = xs.GetEnumerator()
+    while e.MoveNext() do
+      yield e.Current :?> 'T
+  ]
+
+let inline toArray (xs: ^TCollection) : 'T [] when ^TCollection : (member get_Item : int -> 'T) and ^TCollection :> ICollection =
+  [|
+    let e = xs.GetEnumerator()
+    while e.MoveNext() do
+      yield e.Current :?> 'T
+  |]
+
+let nameValueCollectionToMap (x: System.Collections.Specialized.NameValueCollection) : Map<string, string list> =
+  x.AllKeys
+  |> Array.map (fun key -> key, x.GetValues(key) |> Array.toList)
+  |> Map.ofArray
